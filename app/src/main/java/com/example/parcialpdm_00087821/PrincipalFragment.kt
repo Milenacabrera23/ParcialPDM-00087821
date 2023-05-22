451235103162eaf5aa5d08f8076fd8d1eabd5060
andrea.mcabrera <00087821@uca.edu.sv>
2023-05-22 15:31:16 -0600

    Ultimo commit

--- app/src/main/java/com/example/parcialpdm_00087821/PrincipalFragment.kt
+++ app/src/main/java/com/example/parcialpdm_00087821/PrincipalFragment.kt
@@ -1,16 +1,15 @@
 package com.example.parcialpdm_00087821
 
 import android.os.Bundle
-import android.provider.ContactsContract.Data
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class PrincipalFragment : Fragment() {
     private lateinit var editName
+    private Lateinit var
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,15 +34,5 @@ class PrincipalFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_principal, container, false)
-    }
-
-    companion object {
-        fun newInstance(param1: String, param2: String) =
-            PrincipalFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
